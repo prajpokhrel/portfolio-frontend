ccad005cb9904c5b127f9ab936afad672ae0b103
Prajwal Pokhrel <prajwal.pokhrel18@gmail.com>
2021-11-01 12:25:07 +0545

    Updated API url

--- src/axios-portfolio.js
+++ src/axios-portfolio.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:5000/api/v1"
+    baseURL: "https://gory-grave-47999.herokuapp.com/api/v1"
 });
 
 // axios.defaults.withCredentials = true;
